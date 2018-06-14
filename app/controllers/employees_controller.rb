7c38c935568cb3e21ad36cf56a9c847673354195
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-06-14 15:17:44 -0400

    first commit

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,49 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = find_employee
+  end
+
+  def edit
+    @employee = find_employee
+    @all_dogs = Dog.all
+  end
+
+  def update
+    @employee = find_employee
+    if @employee.update(employee_params)
+      redirect_to @employee
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @employee = Employee.new
+    @all_dogs = Dog.all
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.valid?
+      @employee.save
+      redirect_to @employee
+    else
+      render :new
+    end
+  end
+
+  def find_employee
+    Employee.find(params[:id])
+  end
+
+private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :title, :alias, :office, :img_url, :dog_id)
+  end
+
 end
